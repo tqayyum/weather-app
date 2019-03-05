0012fbb9cf43ad156b46302a7ac2e4d5bad86b29
[Tahir Qayyum] <[qayyumtahir00@gmail.com]>
2019-03-04 20:15:27 -0500

    Adding the .gitignore file and a playground file

---
+++ playground/async-basics.js
@@ -0,0 +1,10 @@
+console.log('Starting app.js');
+
+setTimeout(() => {
+    console.log('2 Second Timer');
+}, 2000);
+
+setTimeout(() => {
+    console.log('0 Second Timer');
+}, 0);
+console.log('Finishing up');
